8e9c91878cbe062d24941cd58b6f0843a15e2ca2
WeiTim_mac <speciallife0318@gmail.com>
2022-09-08 11:16:18 +0800

    reorganize

--- main.m
+++ main.m
@@ -1,15 +1,20 @@
 clear; clc;
 %% ------ Global setting ------
-samples_times = 9;       % numbers of extraction of each size from DFN model
-samples_length = 8;
+samples_times = 1;       % numbers of extraction of each size from DFN model
+samples_length = 16;
 
 %% ------input the datas of flow------
-file_path = 'D:\wei_data\Research\REV_determination\REV_determination\REV_without_stress_with_dis\velocity_results\8\';
+%file_path = 'D:\wei_data\Research\REV_determination\REV_determination\REV_without_stress_with_dis\velocity_results\8\';
+% file_path = 'D:\wei_data\Research\REV_determination\REV_determination\REV_without_stress_without_dis\velocity_results\8\';
+file_path = 'D:\wei_data\Research\REV_determination\REV_determination\REV_without_stress_with_dis\velocity_results\16_new\4';
 addpath(file_path);
 header_lines = 5;
-data_bc_x = importdata('velocity_8_x.dat', ' ', header_lines);
-data_bc_y = importdata('velocity_8_y.dat', ' ', header_lines);
-data_bc_z = importdata('velocity_8_z.dat', ' ', header_lines);
+% data_bc_x = importdata('velocity_8_x.dat', ' ', header_lines);
+% data_bc_y = importdata('velocity_8_y.dat', ' ', header_lines);
+% data_bc_z = importdata('velocity_8_z.dat', ' ', header_lines);
+data_bc_x = importdata('velocity_16_x.dat', ' ', header_lines);
+data_bc_y = importdata('velocity_16_y.dat', ' ', header_lines);
+data_bc_z = importdata('velocity_16_z.dat', ' ', header_lines);
 data_bc_x = data_bc_x.data;
 data_bc_y = data_bc_y.data;
 data_bc_z = data_bc_z.data;
@@ -48,7 +53,7 @@ std_error_Krm1 = std_Krm1 / sqrt(len);
 COV_Krm1 = (std_Krm1 ./ mean_Krm1) * 100;
 
 % figure
-Errorbar(samples_times, Krm1, samples_length, len, mean_Krm1, std_Krm1, std_error_Krm1, COV_Krm1);
+%Errorbar(samples_times, Krm1, samples_length, len, mean_Krm1, std_Krm1, std_error_Krm1, COV_Krm1);
 
 % output results
-output_result(samples_length, Kxx, Kyy, Kzz);
+output_result(samples_length, K_tensor_samples);
--- output_result.m
+++ output_result.m
@@ -1,8 +1,10 @@
-function output_result(samples_length, Kxx, Kyy, Kzz);
-Kxx = Kxx'; Kyy = Kyy'; Kzz = Kzz';
-output_table = table(Kxx, Kyy, Kzz);
-
+function output_result(samples_length, K_tensor_samples);
+% Kxx = Kxx'; Kyy = Kyy'; Kzz = Kzz';
+% output_table = table(Kxx, Kyy, Kzz);
+% 
+% file_name = sprintf('size_%d.csv', samples_length);
+% writetable(output_table, file_name, 'Delimiter', ' ');
 file_name = sprintf('size_%d.csv', samples_length);
-writetable(output_table, file_name, 'Delimiter', ' ');
+writematrix(K_tensor_samples, file_name, 'Delimiter',' ')
 
 end
